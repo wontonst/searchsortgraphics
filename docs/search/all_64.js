36bbdeb71d2cd33b8773dbb54574b9b9f331b784
Yiwei Zheng <yiweizhe@usc.edu>
2013-01-19 19:06:11 -0800

    docs

--- docs/search/all_64.js
+++ docs/search/all_64.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['debug',['debug',['../classsearchsortgraphics_1_1_dynamic_screen.html#a1cfad1b7df871d65499a23d701dc8a68',1,'searchsortgraphics.DynamicScreen.debug()'],['../classsearchsortgraphics_1_1_base_g_u_i.html#acbbea3316d6811dee6077d0baca24f85',1,'searchsortgraphics.BaseGUI.debug()']]],
   ['default_5fcolor',['DEFAULT_COLOR',['../classsearchsortgraphics_1_1_dynamic_screen.html#a65c38255003571a798a568583e9dec04',1,'searchsortgraphics::DynamicScreen']]],
+  ['deprecated_20list',['Deprecated List',['../deprecated.html',1,'']]],
   ['displaynumbergenerate',['displayNumberGenerate',['../classsearchsortgraphics_1_1_g_u_i_1_1_main_display.html#a24ec898160688c53ca2f08e782ccf531',1,'searchsortgraphics::GUI::MainDisplay']]],
   ['displaysortchoose',['displaySortChoose',['../classsearchsortgraphics_1_1_g_u_i_1_1_main_display.html#a36aa8eedd43a06ce41959a66b52d3f9a',1,'searchsortgraphics::GUI::MainDisplay']]],
   ['dynamicscreen',['DynamicScreen',['../classsearchsortgraphics_1_1_dynamic_screen.html',1,'searchsortgraphics']]],
