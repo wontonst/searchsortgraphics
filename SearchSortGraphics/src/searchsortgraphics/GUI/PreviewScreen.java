acdf75d64f4ab2d62cae708113437566ad40d3f3
Yiwei Zheng <yiweizhe@usc.edu>
2013-09-11 23:19:59 -0700

    trying to make preview screen

---
+++ SearchSortGraphics/src/searchsortgraphics/GUI/PreviewScreen.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package searchsortgraphics.GUI;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author RoyZheng
+ */
+public class PreviewScreen extends JFrame{
+    
+    public PreviewScreen(){
+        
+    
+        this.setVisible(true);
+    }
+}
